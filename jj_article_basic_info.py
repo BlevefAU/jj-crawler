25522df078a5aba872dff56ca2c434c442d25563
Bleve <wsbaihekong@gmail.com>
2022-06-20 10:55:55 +0800

    1.0.0
    1.0.0

--- jj_article_basic_info.py
+++ jj_article_basic_info.py
@@ -26,8 +26,8 @@ def load_user_agents(file, pattern):
 # pick up pattern in agent list
 agents_list = load_user_agents('user_agents.json', 'Googlebot\\/')
 
-# modify the required number here
-for novelid in range(1724500, 1724505):
+# modify the required number here (start, end)
+for novelid in range(1724503, 1724505):
     url = 'https://www.jjwxc.net/onebook.php?novelid='+str(novelid)
     head = {
         'User-Agent': random.choice(agents_list),
@@ -49,19 +49,30 @@ for novelid in range(1724500, 1724505):
         # if the article data exist
         raw_data.find(id='oneboolt')
         try:
+            # modify here to write the data into file
+            # -----------------------------
+            # title
             print(raw_data.find("span",class_="bigtext").get_text())
+            # description
             print(raw_data.find(id='novelintro').get_text())
+            # article type
             print(raw_data.find("span", itemprop="genre").get_text())
-
             print("________________________")
+            # -----------------------------
+
         except Exception as e:
+            # if encounter error means the article is deleted or other error
             print(e)
 
+        # if you want to import into sql database, uncomment this part and modify it
+        # -----------------------------
         # try:
         #     conn = pymysql.connect(
         #     host='localhost', user='root', passwd='root', db='jjwxc', charset='utf8')   
         # except Exception as e:
         #     print(e)
+        # -----------------------------
+
     except Exception as e:
         # eles print error and pass
         print(e)
